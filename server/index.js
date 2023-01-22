e828d4f11e7e0a80076ad19e36ddd362e5e22acd
khafia ayyub <khafiaayyub@gmail.com>
2023-01-22 23:22:28 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const app = express();  //inialize express as func
 
 const PORT = 8000;
 
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+
 app.listen(PORT,() => console.log(`server is running successfully on PORT ${PORT}`));    
                        //function call //listen func takes two arguments
                         //  1st argument  is PORT number and 2nd argument is call back function
