566ccecf2566dc7f1de499a3e1fcc6675fcb3d7e
khafia ayyub <khafiaayyub@gmail.com>
2023-01-23 23:22:07 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const password = process.env.DB_PASSWORD;
 
 Connection(username, password);
 
+
 app.listen(PORT,() => console.log(`server is running successfully on PORT ${PORT}`));    
                        //function call //listen func takes two arguments
                         //  1st argument  is PORT number and 2nd argument is call back function
