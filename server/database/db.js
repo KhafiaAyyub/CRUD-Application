a50d816d2e45cdc10726412e966744ef1b0fb0a3
khafia ayyub <khafiaayyub@gmail.com>
2023-01-24 18:25:48 +0530

    commit

--- server/database/db.js
+++ server/database/db.js
@@ -1,14 +1,16 @@
 import mongoose from 'mongoose'
 
-const Connection = () => {        //funciton
+const Connection = async () => {        //function
     //with the help of third party library we can connect to database
     //1.mongodb
-    //2.mnognoose (some extra feature)
+    //2.mongnoose (some extra feature)
     const URL = '';
 
     try {
-        mongoose.connect(URL,{ useUnifiedTopology:true,userNewUrlParser:true});
+        await mongoose.connect(URL,{ useUnifiedTopology:true,userNewUrlParser:true});
     } catch(error) {
         console.log('Error while connecting with the database ',error);
     }
 }
+
+export default Connection; 
