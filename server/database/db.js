4787e0b45a1c5eb2891d05b046904886fcf760e5
khafia ayyub <khafiaayyub@gmail.com>
2023-01-24 18:35:51 +0530

    commit

--- server/database/db.js
+++ server/database/db.js
@@ -4,10 +4,11 @@ const Connection = async () => {        //function
     //with the help of third party library we can connect to database
     //1.mongodb
     //2.mongnoose (some extra feature)
-    const URL = '';
-
+    const URL = `mongodb+srv://khafiaayyub_:khafiaayyub<password>@crud-app.lrarmgn.mongodb.net/?retryWrites=true&w=majority`;
+    
     try {
         await mongoose.connect(URL,{ useUnifiedTopology:true,userNewUrlParser:true});
+        console.log('Database connected succesfully');
     } catch(error) {
         console.log('Error while connecting with the database ',error);
     }
