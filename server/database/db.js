782b46bd4ee356395343d573ea829f5ccdd43fd2
khafia ayyub <khafiaayyub@gmail.com>
2023-01-27 22:50:53 +0530

    commit

--- server/database/db.js
+++ server/database/db.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose"  
 
 const Connection = async () => {        //function
     //with the help of third party library we can connect to database
