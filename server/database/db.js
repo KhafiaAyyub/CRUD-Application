e35b6a2606dece80d89d7fb946141db4834fc555
khafia ayyub <khafiaayyub@gmail.com>
2023-02-04 17:01:51 +0530

    commit

--- server/database/db.js
+++ server/database/db.js
@@ -1,20 +1,39 @@
-import mongoose from "mongoose" 
+// import mongoose from 'mongoose';
 
 
-const Connection = async () => {        //function
-    //with the help of third party library we can connect to database
-    //1.mongodb
-    //2.mongnoose (some extra feature)
-    const URL = `mongodb+srv://khafiaayyub_:khafiaayyub@crud-app.lrarmgn.mongodb.net/?retryWrites=true&w=majority`;
+// const Connection = async () => {        //function
+//     //with the help of third party library we can connect to database
+//     //1.mongodb
+//     //2.mongnoose (some extra feature)
+//     const URL = `mongodb+srv://khafiaayyub_:khafiaayyub@crud-app.lrarmgn.mongodb.net/?retryWrites=true&w=majority`;
     
-    try {
-        await mongoose.connect(URL,{ 
-            useUnifiedTopology:true,
-            userNewUrlParser:true});
-        console.log('Database connected succesfully');
-    } catch(error) {
-        console.log('Error while connecting with the database ',error);
-    }
-}
+//     try {
+//         await mongoose.connect(URL,{ 
+//             maxPoolSize: 50,
+//             wtimeoutMS: 2500,
+//             useUnifiedTopology:true,
+//             userNewUrlParser:true});
+//         console.log('Database connected succesfully');
+//     } catch(error) {
+//         console.log('Error while connecting with the database ',error);
+//     }
+// }
 
-export default Connection; 
+// export default Connection;
+
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+const user = process.env.mongoUser;
+const pass = process.env.mongoPass;
+// const url = `;
+  const url = `mongodb+srv://khafiaayyub_:khafiaayyub@crud-app.lrarmgn.mongodb.net/?retryWrites=true&w=majority`;
+
+
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+.then(console.log('connecting'))
+.catch(err => console.log(`error: ${err}`))
